c596d9cd9e8b5a2f074992d99016e1301130dc12
John Boyes <john@agilepathway.co.uk>
2020-12-05 11:55:37 +0000

    Implement method to validate password
    The implementation of the method to validate passwords is now complete,
    but it currently is still fed hardcoded values rather than the actual
    values from the text file.

--- src/com/github/johnboyes/adventofcode2020/Password.java
+++ src/com/github/johnboyes/adventofcode2020/Password.java
@@ -2,12 +2,22 @@
 
 public class Password {
 
-	public Password(String next) {
+	private String passwordValue;
+    private char requiredChar;
+    private int minOccurrences;
+    private int maxOccurrences;
 
+    public Password(String rawPasswordData) {
+        // TODO extract the values from the passwords text file properly
+        passwordValue = "";
+        requiredChar = 'c';
+        minOccurrences = 1;
+        maxOccurrences = 3;
 	}
 
 	public boolean isValid() {
-		return true;
+        long occurrences = passwordValue.chars().filter(ch -> ch == requiredChar).count();
+        return (occurrences >= minOccurrences) && (occurrences <= maxOccurrences);
 	}
 
 }
