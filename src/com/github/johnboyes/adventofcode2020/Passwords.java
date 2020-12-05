4a286229a39a315c199497c9c239978f4fe72e69
John Boyes <john@agilepathway.co.uk>
2020-12-05 11:24:34 +0000

    Load the passwords text file
    The totalValid method is still incorrect - it now returns the total
    number of passwords in the text file, regardless of whether they are
    valid or not.

--- src/com/github/johnboyes/adventofcode2020/Passwords.java
+++ src/com/github/johnboyes/adventofcode2020/Passwords.java
@@ -1,9 +1,29 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Passwords {
 
+    private List<String> passwords = new ArrayList<String>();
+
+    public Passwords() throws FileNotFoundException {
+        loadPasswords();
+    }
+
 	public int totalValid() {
-		return 0;
-	}
+		return passwords.size();
+    }
+    
+    // https://www.baeldung.com/reading-file-in-java#read-file-with-scanner
+    private void loadPasswords() throws FileNotFoundException {
+        String passwordsFile = "resources/com/github/johnboyes/adventofcode2020/day2/passwords.txt";
+        Scanner scanner = new Scanner(new File(passwordsFile));
+        scanner.useDelimiter("\n");
+        while (scanner.hasNext()) {
+            passwords.add(scanner.next());
+        }
+    }
 
 }
