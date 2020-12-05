616ce43dd705f3d1276fe773ee3b8c37ef056a00
John Boyes <john@agilepathway.co.uk>
2020-12-05 09:52:48 +0000

    Complete part 1 day 2

--- src/com/github/johnboyes/adventofcode2020/AdventOfCode2020.java
+++ src/com/github/johnboyes/adventofcode2020/AdventOfCode2020.java
@@ -4,5 +4,6 @@ public class AdventOfCode2020 {
 
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println(new Expenses().multiplyTheTwoEntriesWhichSumTo2020());
+        System.out.println(new Expenses().multiplyTheThreeEntriesWhichSumTo2020());
     }
 }
--- src/com/github/johnboyes/adventofcode2020/Expenses.java
+++ src/com/github/johnboyes/adventofcode2020/Expenses.java
@@ -27,6 +27,30 @@ public class Expenses {
             ).findAny().orElseThrow(() -> new IllegalStateException());
     }
 
+    // loop through each item
+    // loop through all the other items
+    // we still want our original flatmap
+    // the flatmap should now have three items in it, not 2
+
+    public int multiplyTheThreeEntriesWhichSumTo2020() {
+        return findTheThreeExpensesWhichSumTo2020().stream().reduce(1, (a, b) -> a * b);
+    }
+
+
+    private List<Integer> findTheThreeExpensesWhichSumTo2020() {
+        return expenseEntries.stream()
+            .flatMap(
+                value1 -> expenseEntries.stream()
+                    .filter(value2 -> (value1 > value2))
+                    .flatMap(
+                        value2 -> expenseEntries.stream()
+                            .filter(value3 -> (value3 != value2))
+                            .filter(value3 -> (value1 + value2 + value3 == 2020))
+                            .map(value3 -> List.of(value1, value2, value3))
+                    )
+            ).findAny().orElseThrow(() -> new IllegalStateException());
+    }
+
     // https://www.baeldung.com/reading-file-in-java#read-file-with-scanner
     private void initialiseExpenses() throws FileNotFoundException {
         String expenseEntriesFile = "resources/com/github/johnboyes/adventofcode2020/expense-entries.txt";
