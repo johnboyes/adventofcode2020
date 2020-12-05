f31c3d9eb14fba925173566942edaddab30c2b0d
John Boyes <john@agilepathway.co.uk>
2020-12-05 15:32:17 +0000

    Extract password from text file

--- src/com/github/johnboyes/adventofcode2020/Password.java
+++ src/com/github/johnboyes/adventofcode2020/Password.java
@@ -1,23 +1,23 @@
 
-
 public class Password {
 
-	private String passwordValue;
-    private char requiredChar;
-    private int minOccurrences;
-    private int maxOccurrences;
+        private String passwordValue;
+        private char requiredChar;
+        private int minOccurrences;
+        private int maxOccurrences;
 
-    public Password(String rawPasswordData) {
-        // TODO extract the values from the passwords text file properly
-        passwordValue = "";
-        requiredChar = 'c';
-        minOccurrences = 1;
-        maxOccurrences = 3;
-	}
+        public Password(String rawPasswordData) {
+                // TODO extract the values from the passwords text file properly
+                String[] split = rawPasswordData.split(" ");
+                passwordValue = split[2];
+                requiredChar = 'c';
+                minOccurrences = 1;
+                maxOccurrences = 3;
+        }
 
-	public boolean isValid() {
-        long occurrences = passwordValue.chars().filter(ch -> ch == requiredChar).count();
-        return (occurrences >= minOccurrences) && (occurrences <= maxOccurrences);
-	}
+        public boolean isValid() {
+                long occurrences = passwordValue.chars().filter(ch -> ch == requiredChar).count();
+                return (occurrences >= minOccurrences) && (occurrences <= maxOccurrences);
+        }
 
 }
