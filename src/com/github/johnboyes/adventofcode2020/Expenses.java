e20ec8f84a93e16df3d426e3db14528974e818cb
John Boyes <john@agilepathway.co.uk>
2020-12-05 07:06:33 +0000

    Refactor code

--- src/com/github/johnboyes/adventofcode2020/Expenses.java
+++ src/com/github/johnboyes/adventofcode2020/Expenses.java
@@ -5,36 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+
 public class Expenses {
 
-    private List<Integer> expenseEntries;
+    private List<Integer> expenseEntries = new ArrayList<Integer>();
 
     public Expenses() throws FileNotFoundException {
-        expenseEntries = expenseEntries();
+        initialiseExpenses();
     }
 
     public int multiplyTheTwoEntriesWhichSumTo2020() {
+        return findTheTwoExpensesWhichSumTo2020().stream().reduce(1, (a, b) -> a * b);
+    }
+
+    private List<Integer> findTheTwoExpensesWhichSumTo2020() {
         for (int entry : expenseEntries) {
             List<Integer> candidates = expenseEntries.stream().filter(e -> (e != entry)).collect(Collectors.toList());
             for (int candidate : candidates) {
-                if (entry + candidate == 2020) {
-                    return entry * candidate;
-                }
+                if (entry + candidate == 2020) return List.of(entry, candidate);
             }
         }
         throw new IllegalStateException();
     }
 
     // https://www.baeldung.com/reading-file-in-java#read-file-with-scanner
-    private List<Integer> expenseEntries() throws FileNotFoundException {
-        List<Integer> expenseEntries = new ArrayList<Integer>();
+    private void initialiseExpenses() throws FileNotFoundException {
         String expenseEntriesFile = "resources/com/github/johnboyes/adventofcode2020/expense-entries.txt";
         Scanner scanner = new Scanner(new File(expenseEntriesFile));
         scanner.useDelimiter("\n");
         while (scanner.hasNext()) {
             expenseEntries.add(Integer.valueOf(scanner.next()));
         }
-        return expenseEntries;
     }
     
 }
